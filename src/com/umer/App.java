8d8f36b827279826bb5ec791bc642fe50fe1f862
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-08-06 18:09:16 +0200

    Feature: Created basic classes without any logic.

--- src/com/umer/App.java
+++ src/com/umer/App.java
@@ -1,7 +1,11 @@
 package com.umer;
 
+import com.umer.service.LFUCacheService;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        LFUCacheService service = new LFUCacheService();
+        service.testMethod();
     }
 }
--- src/com/umer/service/LFUCacheService.java
+++ src/com/umer/service/LFUCacheService.java
@@ -2,4 +2,7 @@ package com.umer.service;
 
 public class LFUCacheService {
 
+    public String testMethod() {
+        return "message from inside the service";
+    }
 }
