69b3c64d356934cbf442d3719d7bbcd8c4350b85
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-08-07 18:24:41 +0200

    Feature: Implemented the core logic of LFUCacheService.

--- src/com/umer/service/LFUCacheService.java
+++ src/com/umer/service/LFUCacheService.java
@@ -64,4 +64,42 @@ public class LFUCacheService {
         return values.get(key);
     }
 
+    public void put(int key, int value) {
+        // There should be some initial capacity defined.
+        if (capacity <= 0) {
+            return;
+        }
+
+        // Use Case: If the key is already existing...
+        if (values.containsKey(key)) {
+            values.put(key, value);
+            // The following get() method call helps in updating the frequency
+            get(key);
+            return;
+        }
+
+        // Check if we need to evict based on capacity
+        if (values.size() >= capacity) {
+            // Yes, please evict the least frequently used key
+            int evictKey = frequencyMap.get(minFrequency).firstKey();
+            // We remove evictKey from everywhere(i.e. all data structures).
+            frequencyMap.get(minFrequency).remove(evictKey);
+            values.remove(evictKey);
+            frequencies.remove(evictKey);
+            // TODO: Please resrach more about this.
+            if (frequencyMap.get(minFrequency).isEmpty()) {
+                frequencyMap.remove(minFrequency);
+            }
+        }
+
+        // Use case: If we are inserting a new key in the cache
+        values.put(key, value);
+        // This helps in setting up the initial frequency of the key provided in the
+        // method.
+        frequencies.put(key, 1);
+        // TODO: Please resrach more about this.
+        minFrequency = 1;
+        frequencyMap.computeIfAbsent(1, k -> new TreeMap<>()).put(key, value);
+    }
+
 }
